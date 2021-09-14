9c311ee4dc30d1079af52f31360202a792ef730f
renaatosalgado <renaato.salgado@gmail.com>
2021-09-14 20:01:08 -0300

    initial files, all ready to go

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Posts from "./posts/Posts";
 
 import "../reset-css/reset.css"
 
 
 export default function App() {
   return(
-    <Posts />
+    <p>teste bacana</p>
   );
 }
