a2bf096f17b88857c33805df09d977438554df3a
renaatosalgado <renaato.salgado@gmail.com>
2021-09-15 20:45:45 -0300

    a saga

--- src/Pages/PageLogin.js
+++ src/Pages/PageLogin.js
@@ -1,67 +1,102 @@
-import { useState } from "react";
-import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Container, ContainerTitle ,ContainerForm, Title, Description, Input, Button, Connection } from './Shared-Styled-Login-Registration';
+import {
+  Container,
+  ContainerTitle,
+  ContainerForm,
+  Title,
+  Description,
+  Input,
+  Button,
+  Connection,
+} from '../styled-components/Shared-Styled-Login-Registration'
 import Swal from "sweetalert2";
+import UserContext from "../contexts/UserContext";
+
+import { postLogin } from "../services/API";
 
 export default function PageLogin() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [condition, setCondition] = useState(false)
-    const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [condition, setCondition] = useState(false);
+  const history = useHistory();
 
-    function SendLoginInformation(event) {
-        event.preventDefault()
-        const body = {
-            email,
-            password
-        }
+  const { user, setUser } = useContext(UserContext);
 
-        function Error(res) {
-            if (res.response.status === 400){
-                Swal.fire({
-                    icon: 'error',
-                    title: 'OOPS...',
-                    text: 'Seu email é invalido',
-                  })
-            }
-            if(res.response.status === 403){
-                Swal.fire({
-                    icon: 'error',
-                    title: 'OOPS...',
-                    text: 'Seus dados não foram encontrados 🤔, verifica se está tudo certo aí 😉, se não tiver uma conta se cadastre 🙂',
-                  })
-            }
-        }
-        setCondition(true)
-        axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/sign-in', body)
-        .then (() => { history.push('/timeline')
-            setCondition(false);
-        })
-        .catch ( res => {Error(res)
-        setCondition(false)})
-    }
+  function SendLoginInformation(event) {
+    event.preventDefault();
 
-    return(
-        <form onSubmit={SendLoginInformation}>
-            <Container>
-                <ContainerTitle>
-                    <Title>linkr</Title>
-                    <Description>
-                    save, share and discover <br/>
-                    the best links on the web
-                    </Description>
-                </ContainerTitle>
-                <ContainerForm>
-                    <Input placeholder='e-mail' type='email' value={email} onChange= {e => setEmail(e.target.value)} required/>
-                    <Input placeholder='password' type='password' value={password} onChange= {e => setPassword(e.target.value)} required/>
-                    <Button type='submit' disabled={condition}>Log In</Button>
-                    <Link to='/sign-up'>
-                        <Connection>First time? Create an account!</Connection>
-                    </Link>
-                </ContainerForm>
-            </Container>
-        </form>
-    );
+    const body = {
+      email,
+      password,
+    };
+
+    postLogin(body)
+      .then((res) => {
+        setUser(res.data);
+        setCondition(false);
+        history.push("/timeline");
+      })
+      .catch((res) => {
+        Error(res);
+        setCondition(false);
+      });
+
+     function Error(res) {
+         console.log(res)
+       if (res.response.status === 400) {
+         Swal.fire({
+           icon: "error",
+           title: "OOPS...",
+           text: "Seu email é invalido",
+         });
+       }
+       if (res.response.status === 403) {
+         Swal.fire({
+           icon: "error",
+           title: "OOPS...",
+           text: "Seus dados não foram encontrados 🤔, verifica se está tudo certo aí 😉, se não tiver uma conta se cadastre 🙂",
+         });
+       }
+     }
+     setCondition(true);
+    console.log(user)
+  }
+
+  return (
+    <form onSubmit={SendLoginInformation}>
+      <Container>
+        <ContainerTitle>
+          <Title>linkr</Title>
+          <Description>
+            save, share and discover <br />
+            the best links on the web
+          </Description>
+        </ContainerTitle>
+        <ContainerForm>
+          <Input
+            placeholder="e-mail"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <Button type="submit" disabled={condition}>
+            Log In
+          </Button>
+          <Link to="/sign-up">
+            <Connection>First time? Create an account!</Connection>
+          </Link>
+        </ContainerForm>
+      </Container>
+    </form>
+  );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,29 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import React from "react";
-import "../reset-css/reset.css"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useContext, useState } from "react";
 import PageLogin from "../Pages/PageLogin";
-import PageRegistration from '../Pages/PageRegistration';
-import Timeline from './timeline/Timeline';
+import PageRegistration from "../Pages/PageRegistration";
+import PageTimeline from "../Pages/PageTimeline";
+import UserContext from "../contexts/UserContext";
+import Reset from "../styled-components/Reset";
 
 export default function App() {
-  return(
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact>
-          <PageLogin/>
-        </Route>
-        <Route path='/sign-up' exact>
-          <PageRegistration/>
-        </Route>
-        <Route path='/timeline'exact>
-          <Timeline/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+  const [user, setUser] = useState('');
+  return (
+    <UserContext.Provider value={{user, setUser}}>
+      <Reset />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <PageLogin />
+          </Route>
+          <Route path="/sign-up" exact>
+            <PageRegistration />
+          </Route>
+          <Route path="/timeline" exact>
+            <PageTimeline />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
