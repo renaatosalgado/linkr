3c4571b81c5ed5163d7c619c343d87df27ccb747
patriciasilva.eq@gmail.com <patriciasilva.eq@gmail.com>
2021-09-17 09:57:36 -0400

    Acesso rápido às rotas my posts, my likes e logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,42 @@
 import React from "react";
 import styled from "styled-components";
+import { FaChevronDown } from "react-icons/fa";
+import UserContext from "../contexts/UserContext";
+import { useContext, useState } from "react";
+import { useHistory, Link } from "react-router-dom";
 
 export default function Header() {
+  const [quickAccess, setQuickAccess] = useState(false)
+  const {
+    user,
+    setUser
+  } = useContext(UserContext)
+
+  const history = useHistory();
+
+  function showQuickAccess() {
+    if (quickAccess) {
+      setQuickAccess(false)
+    } else {
+      setQuickAccess(true)
+    }
+  }
+
+  function logout() {
+    
+    history.push("/");
+  }
+  
   return (
     <HeaderContainer>
       <p>linkr</p>
+      <img onClick={showQuickAccess} src={user.user.avatar} alt="foto de perfil" />
+      <ChevronIcon quickAccess={quickAccess} onClick={showQuickAccess} size="25px" />
+      <DivQuickAccess quickAccess={quickAccess}>
+        <Link to="/my-posts">My posts</Link>
+        <Link to="/my-likes">My likes</Link>
+        <h3 onClick={logout}>Logout</h3>
+      </DivQuickAccess>
     </HeaderContainer>
   );
 }
@@ -21,11 +53,21 @@ const HeaderContainer = styled.div`
   top: 0;
   left: 0;
   z-index: 1;
+  position: relative;
 
   p {
     font-family: "Passion One", cursive;
     font-weight: bold;
     font-size: 49px;
+  }
+  
+  img {
+    width: 53px;
+    height: 53px;
+    position: absolute;
+    top: 10px;
+    right: 17px;
+    border-radius: 50%;
   }
 
   @media(max-width: 635px){
@@ -35,4 +77,33 @@ const HeaderContainer = styled.div`
     padding-left: 17px;
   }
 
+`;
+
+const ChevronIcon = styled(FaChevronDown)`
+  position: absolute;
+  right: 86.31px;
+  top: 32.38px;
+  transform: ${({quickAccess}) => quickAccess ? "rotate(180deg)" : "rotate(0deg)"};
+`;
+
+const DivQuickAccess= styled.div`
+    width: 135px;
+     height: 109px;
+     background-color: #171717;
+    border-bottom-left-radius: 20px;
+    display: flex;
+    flex-direction: column;
+     align-items: center;
+    position: absolute;
+     top: 72px;
+     right: 0;
+     opacity: ${({quickAccess}) => quickAccess ? 1 : 0};
+
+     h3, a {
+     font-size: 17px;
+     color: #FFFFFF;
+     font-weight: bold;
+     margin-top: 12px;
+     font-family: 'Lato', sans-serif;
+   }
 `;
